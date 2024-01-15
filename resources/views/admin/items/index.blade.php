0c11e9e46a27d14eb9287bdda375b2c08cc36d88
matteobonelli <matteobonelli08@gmail.com>
2024-01-15 16:01:47 +0100

    items route updated

--- resources/views/admin/items/index.blade.php
+++ resources/views/admin/items/index.blade.php
@@ -11,7 +11,7 @@
             {{session('message')}}
         </div>
         @endif
-        <a href="{{ route('items.create') }}" class="btn btn-primary my-3">Create a new item</a>
+        <a href="{{ route('admin.items.create') }}" class="btn btn-primary my-3">Create a new item</a>
         <h1 class="text-light py-3">List Items</h1>
         <div class="row gy-4">
           @foreach ($items as $item)
@@ -22,9 +22,9 @@
                         <h6>slug:{{$item->slug}} | category:{{$item->category}} | type:{{$item->type}}</h6>
                         <span>weight:{{$item->weight}} </span> <span>cost: {{$item->cost}}</span>
                         <p class="card-text">{!! substr($item->description, 0, 100) . '...' !!}</p>
-                        <a href="{{route('items.show', $item->id)}}" class="btn btn-primary">Vedi dettaglio</a>
-                        <a href="{{ route('items.edit', $item->id) }}" class="btn btn-secondary">Modifica</a>
-                        <form action="{{route('items.destroy',$item->id)}}" method="POST">
+                        <a href="{{route('admin.items.show', $item->id)}}" class="btn btn-primary">Vedi dettaglio</a>
+                        <a href="{{ route('admin.items.edit', $item->id) }}" class="btn btn-secondary">Modifica</a>
+                        <form action="{{route('admin.items.destroy',$item->id)}}" method="POST">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="cancel-button btn btn-danger" data-item-title="{{$item->name}}">Cancella</button>
