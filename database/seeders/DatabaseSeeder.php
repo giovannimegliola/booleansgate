4e2b432feb8ad07e6ff378858074187e5403ac08
matteobonelli <matteobonelli08@gmail.com>
2024-01-18 16:26:42 +0100

    seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            ItemSeeder::class,
+            TypeSeeder::class,
+            CharacterSeeder::class,
+            UserSeeder::class
+        ]);
     }
+
 }
